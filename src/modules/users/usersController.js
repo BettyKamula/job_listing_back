418ad5d6df4472065be94488dc96daef62d908b4
Elizabeth Kamula <bettykamula@gmail.com>
2024-09-24 10:14:26 +0300

    feature(users) - implement update user -implement delete user

--- src/modules/users/usersController.js
+++ src/modules/users/usersController.js
@@ -1,4 +1,5 @@
 import User from '../../db/models/user';
+import { handleResponse } from '../../helpers/response';
 
 export class UsersController {
   static async createUser(req, res) {
@@ -34,6 +35,56 @@ export class UsersController {
           message: 'internal server error',
         });
       }
+    }
+  }
+  static async getUsers(req, res) {
+    try {
+      const search = req.query.search;
+      let query = {};
+      if (search) {
+        query.$or = [{ firstName: { $regex: search, $options: 'i' } }];
+      }
+      const users = await User.find(query);
+      return handleResponse(
+        res,
+        200,
+        true,
+        'users fetched successfully',
+        users
+      );
+    } catch (e) {
+      return handleResponse(res, 500, false, 'oops something went wrong');
+    }
+  }
+  static async updateUser(req, res) {
+    const { userId } = req.params;
+    const options = { new: true };
+
+    try {
+      const user = await User.findById({ _id: userId });
+      if (!user) {
+        return handleResponse(res, 400, false, 'user not found');
+      }
+      const data = await User.findByIdAndUpdate(userId, req.body, options);
+      return handleResponse(res, 200, true, 'updated successfully', data);
+    } catch (e) {
+      return handleResponse(res, 500, false, 'Oops something went wrong');
+    }
+  }
+  static async deleteUser(req, res) {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById({ _id: userId });
+      console.log(user);
+      if (!user) {
+        return handleResponse(res, 400, false, 'The user does not exist');
+      }
+      console.log('user', user);
+      user.isActive = false;
+      user.save();
+      return handleResponse(res, 200, true, 'deleted Successfully');
+    } catch (e) {
+      return handleResponse(res, 500, false, 'Oops something went wrong');
     }
   }
 }
